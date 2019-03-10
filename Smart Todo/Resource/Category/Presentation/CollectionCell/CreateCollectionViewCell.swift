1d3cece7725c07144b57ff9dd4f7d98e6c5ddcb0
I MARIE <marie@tt-no-macbook-air.local>
2019-03-10 14:57:41 +0900

    createViewのデザイン設定

--- Smart Todo/Resource/Category/Presentation/CollectionCell/CreateCollectionViewCell.swift
+++ Smart Todo/Resource/Category/Presentation/CollectionCell/CreateCollectionViewCell.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class CreateCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var createImageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        contentView.layer.borderWidth = 0.5
+        contentView.layer.borderColor = UIColor(red: 188/255, green: 189/255, blue: 194/255, alpha: 1.0).cgColor
+        contentView.layer.cornerRadius = 6.0
+        contentView.clipsToBounds = true
+        
+        backView.layer.borderWidth = 0.5
+        backView.layer.borderColor = UIColor.gray.cgColor
+        backView.layer.cornerRadius = backView.frame.width / 2
+        backView.clipsToBounds = true
+        
+        createImageView.image = UIImage.fontAwesomeIcon(name: .plus, style: .solid, textColor: .gray, size: CGSize(width: 30, height: 30))
     }
 
 }
