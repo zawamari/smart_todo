0d90983f6e57c7e9047cdb78c72cf0035120b351
I MARIE <marie@tt-no-macbook-air.local>
2019-03-10 14:36:56 +0900

    collectionCell add

---
+++ Smart Todo/Resource/Category/Presentation/CollectionCell/CreateCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CreateCollectionViewCell.swift
+//  Smart Todo
+//
+//  Created by Marie on 2019/03/10.
+//  Copyright © 2019 Marie. All rights reserved.
+//
+
+import UIKit
+
+class CreateCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
