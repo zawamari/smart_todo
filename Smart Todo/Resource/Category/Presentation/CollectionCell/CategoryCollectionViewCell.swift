c4b21d6240bacc411bde689be6eef10aee394380
I MARIE <marie@tt-no-macbook-air.local>
2019-03-11 23:27:31 +0900

    色をとりあえず固定で

--- Smart Todo/Resource/Category/Presentation/CollectionCell/CategoryCollectionViewCell.swift
+++ Smart Todo/Resource/Category/Presentation/CollectionCell/CategoryCollectionViewCell.swift
@@ -14,63 +14,7 @@ class CategoryCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var categoryIconImageView: UIImageView!
     @IBOutlet weak var categoryNameLabel: UILabel!
     @IBOutlet weak var tasksLabel: UILabel!
-    
-    let colors = [UIColor(red: 211/255, green: 212/255, blue: 155/255, alpha: 1.0), //淡い黄色
-                  UIColor(red: 223/255, green: 188/255, blue: 159/255, alpha: 1.0), //洗柿
-                  UIColor(red: 183/255, green: 196/255, blue:  22/255, alpha: 1.0), //水色
-                  UIColor(red: 239/255, green: 193/255, blue: 196/255, alpha: 1.0), //ベビーピンク
-                  UIColor(red: 162/255, green: 219/255, blue: 168/255, alpha: 1.0), //浅緑
-                  UIColor(red: 154/255, green: 136/255, blue: 164/255, alpha: 1.0), //薄鼠
-                  UIColor(red: 188/255, green: 189/255, blue: 194/255, alpha: 1.0), //スカイグレー
-                  UIColor(red: 114/255, green: 140/255, blue:  12/255, alpha: 1.0), //利休鼠
-                  UIColor(red: 224/255, green: 195/255, blue: 132/255, alpha: 1.0), //伽羅色
-                  UIColor(red: 208/255, green: 205/255, blue: 184/255, alpha: 1.0), //生成
-        UIColor(red: 211/255, green: 212/255, blue: 155/255, alpha: 1.0), //淡い黄色
-        UIColor(red: 223/255, green: 188/255, blue: 159/255, alpha: 1.0), //洗柿
-        UIColor(red: 183/255, green: 196/255, blue:  22/255, alpha: 1.0), //水色
-        UIColor(red: 239/255, green: 193/255, blue: 196/255, alpha: 1.0), //ベビーピンク
-        UIColor(red: 162/255, green: 219/255, blue: 168/255, alpha: 1.0), //浅緑
-        UIColor(red: 154/255, green: 136/255, blue: 164/255, alpha: 1.0), //薄鼠
-        UIColor(red: 188/255, green: 189/255, blue: 194/255, alpha: 1.0), //スカイグレー
-        UIColor(red: 114/255, green: 140/255, blue:  12/255, alpha: 1.0), //利休鼠
-        UIColor(red: 224/255, green: 195/255, blue: 132/255, alpha: 1.0), //伽羅色
-        UIColor(red: 223/255, green: 188/255, blue: 159/255, alpha: 1.0), //洗柿
-        UIColor(red: 183/255, green: 196/255, blue:  22/255, alpha: 1.0), //水色
-        UIColor(red: 239/255, green: 193/255, blue: 196/255, alpha: 1.0), //ベビーピンク
-        UIColor(red: 162/255, green: 219/255, blue: 168/255, alpha: 1.0), //浅緑
-        UIColor(red: 154/255, green: 136/255, blue: 164/255, alpha: 1.0), //薄鼠
-        UIColor(red: 188/255, green: 189/255, blue: 194/255, alpha: 1.0), //スカイグレー
-        UIColor(red: 114/255, green: 140/255, blue:  12/255, alpha: 1.0), //利休鼠
-        UIColor(red: 224/255, green: 195/255, blue: 132/255, alpha: 1.0), //伽羅色
-        UIColor(red: 208/255, green: 205/255, blue: 184/255, alpha: 1.0), //生成
-        UIColor(red: 211/255, green: 212/255, blue: 155/255, alpha: 1.0), //淡い黄色
-        UIColor(red: 223/255, green: 188/255, blue: 159/255, alpha: 1.0), //洗柿
-        UIColor(red: 183/255, green: 196/255, blue:  22/255, alpha: 1.0), //水色
-        UIColor(red: 239/255, green: 193/255, blue: 196/255, alpha: 1.0), //ベビーピンク
-        UIColor(red: 162/255, green: 219/255, blue: 168/255, alpha: 1.0), //浅緑
-        UIColor(red: 154/255, green: 136/255, blue: 164/255, alpha: 1.0), //薄鼠
-        UIColor(red: 188/255, green: 189/255, blue: 194/255, alpha: 1.0), //スカイグレー
-        UIColor(red: 114/255, green: 140/255, blue:  12/255, alpha: 1.0), //利休鼠
-        UIColor(red: 224/255, green: 195/255, blue: 132/255, alpha: 1.0), //伽羅色
-        UIColor(red: 223/255, green: 188/255, blue: 159/255, alpha: 1.0), //洗柿
-        UIColor(red: 183/255, green: 196/255, blue:  22/255, alpha: 1.0), //水色
-        UIColor(red: 239/255, green: 193/255, blue: 196/255, alpha: 1.0), //ベビーピンク
-        UIColor(red: 162/255, green: 219/255, blue: 168/255, alpha: 1.0), //浅緑
-        UIColor(red: 154/255, green: 136/255, blue: 164/255, alpha: 1.0), //薄鼠
-        UIColor(red: 188/255, green: 189/255, blue: 194/255, alpha: 1.0), //スカイグレー
-        UIColor(red: 114/255, green: 140/255, blue:  12/255, alpha: 1.0), //利休鼠
-        UIColor(red: 224/255, green: 195/255, blue: 132/255, alpha: 1.0), //伽羅色
-        UIColor(red: 208/255, green: 205/255, blue: 184/255, alpha: 1.0), //生成
-        UIColor(red: 211/255, green: 212/255, blue: 155/255, alpha: 1.0), //淡い黄色
-        UIColor(red: 223/255, green: 188/255, blue: 159/255, alpha: 1.0), //洗柿
-        UIColor(red: 183/255, green: 196/255, blue:  22/255, alpha: 1.0), //水色
-        UIColor(red: 239/255, green: 193/255, blue: 196/255, alpha: 1.0), //ベビーピンク
-        UIColor(red: 162/255, green: 219/255, blue: 168/255, alpha: 1.0), //浅緑
-        UIColor(red: 154/255, green: 136/255, blue: 164/255, alpha: 1.0), //薄鼠
-        UIColor(red: 188/255, green: 189/255, blue: 194/255, alpha: 1.0), //スカイグレー
-        UIColor(red: 114/255, green: 140/255, blue:  12/255, alpha: 1.0), //利休鼠
-        UIColor(red: 224/255, green: 195/255, blue: 132/255, alpha: 1.0), //伽羅色
-    ]
+    let skygray = UIColor(red: 188/255, green: 189/255, blue: 194/255, alpha: 1.0)
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -89,7 +33,7 @@ extension CategoryCollectionViewCell {
         categoryNameLabel.text = name
         if let count = tasks {
             tasksLabel.text = String(count) + " tasks"
-            categoryIconImageView.image = UIImage.fontAwesomeIcon(name: .circle, style: .solid, textColor: colors[count], size: CGSize(width: 20, height: 20))
+            categoryIconImageView.image = UIImage.fontAwesomeIcon(name: .circle, style: .solid, textColor: skygray, size: CGSize(width: 20, height: 20))
         }
     }
 }
