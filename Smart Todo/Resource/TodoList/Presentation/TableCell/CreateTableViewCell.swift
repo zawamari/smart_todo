f7dab79067b520abfa76f96ca62eea957b8df312
I MARIE <marie@tt-no-macbook-air.local>
2019-03-13 01:01:49 +0900

    todoの影やめる

--- Smart Todo/Resource/TodoList/Presentation/TableCell/CreateTableViewCell.swift
+++ Smart Todo/Resource/TodoList/Presentation/TableCell/CreateTableViewCell.swift
@@ -18,15 +18,17 @@ class CreateTableViewCell: UITableViewCell {
         addImageView.image = UIImage.fontAwesomeIcon(name: .plus, style: .solid, textColor: .gray, size: CGSize(width: 20, height: 20))
         addImageView.clipsToBounds = true
         
+        backView.layer.borderColor = UIColor.gray.cgColor
+        backView.layer.borderWidth = 0.5
         backView.layer.cornerRadius = 6.0
         backView.clipsToBounds = true
         // 影をつける
-        backView.layer.cornerRadius = 10.0
-        backView.layer.masksToBounds = false
-        backView.layer.shadowColor = UIColor.black.cgColor
-        backView.layer.shadowOpacity = 0.25
-        backView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
-        backView.layer.shadowRadius = 3
+//        backView.layer.cornerRadius = 10.0
+//        backView.layer.masksToBounds = false
+//        backView.layer.shadowColor = UIColor.black.cgColor
+//        backView.layer.shadowOpacity = 0.25
+//        backView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+//        backView.layer.shadowRadius = 3
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- Smart Todo/Resource/TodoList/Presentation/TableCell/TodoListTableViewCell.swift
+++ Smart Todo/Resource/TodoList/Presentation/TableCell/TodoListTableViewCell.swift
@@ -25,17 +25,18 @@ class TodoListTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-
+        backView.layer.borderColor = UIColor.gray.cgColor
+        backView.layer.borderWidth = 0.5
         backView.layer.cornerRadius = 6.0
         backView.clipsToBounds = true
         
         // 影をつける
-        backView.layer.cornerRadius = 10.0
-        backView.layer.masksToBounds = false
-        backView.layer.shadowColor = UIColor.black.cgColor
-        backView.layer.shadowOpacity = 0.25
-        backView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
-        backView.layer.shadowRadius = 3
+//        backView.layer.cornerRadius = 10.0
+//        backView.layer.masksToBounds = false
+//        backView.layer.shadowColor = UIColor.black.cgColor
+//        backView.layer.shadowOpacity = 0.25
+//        backView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+//        backView.layer.shadowRadius = 3
         
         let check = UITapGestureRecognizer(target: self, action: #selector(tapCheck(_:)))
         tapView.addGestureRecognizer(check)
