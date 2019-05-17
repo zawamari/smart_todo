840b8beff65904e2e408645fe6856f5884ba0271
Marie <marie@TT-no-MacBook-Air.local>
2019-05-18 00:00:50 +0900

    setting画面土台づくり

--- Smart Todo/Resource/Others/SettingViewController.swift
+++ Smart Todo/Resource/Others/SettingViewController.swift
@@ -1,7 +1,11 @@
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let contents = [0:"laungage", 1:"share", 2:"review", 3: "お問い合わせ" ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -10,7 +14,7 @@ class SettingViewController: UIViewController {
         
         self.navigationController?.navigationBar.tintColor = .black
         
-        let closedImage = UIImage.fontAwesomeIcon(name: .cross, style: .solid, textColor: .gray, size: CGSize(width: 25, height: 25))
+        let closedImage = UIImage.fontAwesomeIcon(name: .times, style: .solid, textColor: .gray, size: CGSize(width: 25, height: 25))
         let settingButtonItem = UIBarButtonItem(image: closedImage, style: .plain, target: self, action: #selector(didTapClosed))
         self.navigationItem.leftBarButtonItem = settingButtonItem
     }
@@ -19,6 +23,26 @@ class SettingViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "settingCell", for: indexPath)
+        cell.textLabel!.text = contents[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            print("0")
+        case 1:
+            print("1")
+        default:
+            print("default")
+        }
+    }
 }
 
 extension SettingViewController {
